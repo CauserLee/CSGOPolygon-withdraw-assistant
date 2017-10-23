95f30f8d7f22f988b0dd908910c6e55cfe8737d7
Luna <arthu.lee.ac72@outlook.com>
2017-10-23 17:56:34 +0800

    Daily sync Luna

--- assets/js/withdraw.js
+++ assets/js/withdraw.js
@@ -1,4 +1,36 @@
-var target   = document.querySelector("#left");
-var observer = new MutationObserver(function () {
-
+let reals    = document.querySelector(".reals");
+let previous = new Map();
+let next     = new Map();
+let target   = document.querySelector("#left");
+let config   = {attributes: true, childList: true, characterData: true, subtree: true};
+let observer = new MutationObserver(function () {
+    if (reals.textContent !== "") {
+        if (previous.has("timestamp")) {
+            if (next.has("timestamp")) {
+                previous = next;
+                next.clear();
+                setTimestamp(next);
+                fillMap(next);
+            } else {
+                setTimestamp(next);
+                fillMap(next);
+            }
+        } else {
+            setTimestamp(previous);
+            fillMap(previous);
+        }
+    }
 });
+
+observer.observe(target, config);
+
+function setTimestamp(map) {
+    map.set("timestamp", new Date().getTime());
+}
+
+function fillMap(map) {
+    for (let i = 0; i < reals.length; i++) {
+        map.set(reals.childNodes[i].childNodes[0].getAttribute("data-pos"), reals.childNodes[i].childNodes[0].getAttribute("data-view"));
+    }
+}
+
